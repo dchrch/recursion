73ab1d4847d66eb7952368e6f9625c471e911603
David Churchill <dchurchill92@gmail.com>
2015-07-29 03:56:40 -0500

    implement stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,21 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function (obj) {
+  if (obj === null || obj === NaN) {
+    return 'null';
+  } else if (typeof obj === 'object') {
+    var res = _.reduce(obj, function(acc, val, key) {
+      val = stringifyJSON(val);
+      if (Array.isArray(obj)) {
+        return val === undefined ? acc.concat(null) : acc.concat(val);
+      }
+      return val === undefined ? acc : acc.concat('"' + key + '":' + val);
+    }, []);
+    return Array.isArray(obj) ? "[" + res + "]" : "{" + res + "}";
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === 'number' || typeof obj === 'boolean' ) {
+    return obj.toString();
+  }  
 };
